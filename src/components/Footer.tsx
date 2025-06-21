853854a873fc9caca5775399c53aaa56a277f45f
Simon <simoncun2586@gmail.com>
2025-06-20 19:21:36 -0700

    fixing page formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import HorizontalTitle from "@/components/HorizontalTitle";
 const Footer = () => {
   return (
     <section
-      id="experience"
+      id="contact"
       className="flex h-screen w-full border-b-1 py-20 pl-8"
     >
       <HorizontalTitle title="Contact" />
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -2,7 +2,10 @@ import HorizontalTitle from "@/components/HorizontalTitle";
 
 const Experience = () => {
   return (
-    <section className="creen flex h-screen w-full justify-end border-b-1 py-20 pr-20">
+    <section
+      id="experience"
+      className="flex h-screen w-full justify-end border-b-1 py-20 pr-20"
+    >
       <HorizontalTitle title="Experience" />
     </section>
   );
