55885592981aa675ff270abf61bd4707b31402ca
Simon <simoncun2586@gmail.com>
2025-06-22 07:57:13 -0700

    installed motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ScrollLink from "@/components/ScrollLink";
 
 const Navbar = () => {
   return (
-    <div className="sticky top-0 z-50 w-full bg-[rgba(0,0,0,0.2)] py-3 text-sm drop-shadow-[0_0_50px_white]">
+    <div className="hidden md:block sticky top-0 z-50 w-full bg-[rgba(0,0,0,0.2)] py-3 text-sm drop-shadow-[0_0_50px_white]">
       <div className="relative flex items-center justify-center">
         <ScrollLink
           to="#home"
