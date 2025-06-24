8163b89e8d694c515d5f9aca375f003dc29ca0ce
Simon <simoncun2586@gmail.com>
2025-06-23 22:58:07 -0700

    formatting

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,15 +6,15 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="left-0 md:flex w-full justify-center border-b-1 py-20 pb-52 md:pb-96 md:pl-10"
+      className="left-0 w-full justify-center border-b-1 py-20 pb-52 md:flex md:pb-96 md:pl-10"
     >
       <div className="sticky">
         <div className="sticky top-0 hidden md:top-[50px] md:block">
           <VerticalTitle title="PROJECTS" />
         </div>
       </div>
-      <div className="sticky md:hidden top-[50px]">
-        <div className="flex sticky top-[50px] w-full justify-center pb-5">
+      <div className="sticky top-[50px] md:hidden">
+        <div className="sticky top-[50px] flex w-full justify-center pb-5">
           <HorizontalTitle title="PROJECTS" />
         </div>
       </div>
@@ -38,7 +38,7 @@ const Projects = () => {
             ) => (
               <div
                 key={index}
-                className={`sticky m-3 rounded-4xl border border-white/20 p-5 text-center shadow-lg backdrop-blur-md backdrop-filter top-[150px] md:top-[50px] md:m-10 md:p-10 md:text-left ${color} flex flex-col gap-6 text-xs md:flex-row`}
+                className={`sticky top-[150px] m-3 rounded-4xl border border-white/20 p-5 text-center shadow-lg backdrop-blur-md backdrop-filter md:top-[50px] md:m-10 md:p-10 md:text-left ${color} flex flex-col gap-6 text-xs md:flex-row`}
                 style={{
                   transform: `translateY(${index * 24}px)`,
                   zIndex: ProjectData.length + index,
@@ -46,7 +46,7 @@ const Projects = () => {
               >
                 <div className="flex flex-col gap-2 md:gap-4">
                   <div className="text-4xl">{title}</div>
-                  <div className="flex gap-5 w-full justify-center md:justify-normal">
+                  <div className="flex w-full justify-center gap-5 md:justify-normal">
                     <div>{role}</div>
                     <div>{date}</div>
                     <div>{status}</div>
@@ -68,12 +68,12 @@ const Projects = () => {
                     <div>{Links.github}</div>
                   </div>
                 </div>
-                <div className="w-full md:w-auto flex justify-center md:justify-normal">
+                <div className="flex w-full justify-center md:w-auto md:justify-normal">
                   <Image
-                  src={image.src}
-                  alt={image.alt}
-                  className="object-contain flex w-[400px]"
-                />
+                    src={image.src}
+                    alt={image.alt}
+                    className="flex w-[400px] object-contain"
+                  />
                 </div>
               </div>
             ),
