2b75ff22189868c0719ae49ab580aac1d1b0ecba
Simon <simoncun2586@gmail.com>
2025-06-26 00:44:47 -0700

    update

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,29 +4,29 @@ import example from "@/public/placeholder.webp";
 export const ExperienceData = [
   {
     logo: example,
-    title: "Example Company A",
+    title: "Example",
     job: "Example Role A",
     information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris, sed tempor velit. Vivamus efficitur turpis at massa dictum, nec rhoncus sem euismod.",
   },
   {
     logo: example,
-    title: "Example Company B",
-    job: "Example Role B",
+    title: "Example",
+    job: "Example",
     information:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
   },
   {
     logo: example,
-    title: "Example Company C",
-    job: "Example Role C",
+    title: "Example",
+    job: "Example",
     information:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque. Corrupti quos dolores et quas molestias excepturi sint occaecati.",
   },
   {
     logo: example,
-    title: "Example Company D",
-    job: "Example Role D",
+    title: "Example",
+    job: "Example",
     information:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.",
   },
