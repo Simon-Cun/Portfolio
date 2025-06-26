6c5a01d76c5e5835906aa207b17621152b2c023f
Simon <simoncun2586@gmail.com>
2025-06-25 22:28:03 -0700

    updating game console

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -7,25 +7,32 @@ import Image from "next/image";
 const Experience = () => {
   const { selectedExperience, setSelectedExperience, ExperienceData } =
     useExperienceContent();
-  const buttons = ["X", "Y", "A", "B"];
+  const buttons = ["X", "A", "Y", "B"];
   return (
     <section
       id="experience"
       className="flex w-full items-center justify-center gap-16 border-b-1 py-20 pr-20"
     >
-      <div className="flex h-[350px] w-[800px] items-center justify-between rounded-4xl bg-purple-200 px-6">
+      <div className="relative flex h-[350px] w-[800px] items-center justify-between rounded-[4rem] bg-purple-200 px-6">
+        <div className="absolute -top-2 -right-1 -z-20 h-20 w-30 rounded-tl-md rounded-tr-[4rem] rounded-b-md border-4 border-white bg-gray-200 shadow-md"></div>
+        <div className="absolute -top-2 -left-1 -z-20 h-20 w-30 rounded-tl-[4rem] rounded-tr-md rounded-b-md border-4 border-white bg-gray-200 shadow-md"></div>
+
         <div className="flex h-full flex-col items-center justify-between py-20">
-          <div className="h-[70px] w-[70px] rounded-full border-8 border-white bg-gray-100"></div>
+          <div className="h-[70px] w-[70px] rounded-full border-8 border-white bg-gray-100 drop-shadow-[0_0_2px_black]"></div>
           <div className="relative h-16 w-16">
-            <div className="absolute top-0 left-1/2 h-full w-5 -translate-x-1/2 bg-white outline-3 outline-black/60" />
-            <div className="absolute top-1/2 left-0 h-5 w-full -translate-y-1/2 bg-white outline-3 outline-black/60" />
+            <div className="absolute top-0 left-1/2 h-full w-5 -translate-x-1/2 bg-white outline-3 outline-gray-500" />
+            <div className="absolute top-1/2 left-0 h-5 w-full -translate-y-1/2 bg-white outline-3 outline-gray-500" />
             <div className="absolute top-0 left-1/2 h-full w-5 -translate-x-1/2 bg-white" />
           </div>
         </div>
-        <div className="flex h-[330px] w-[550px] flex-col rounded-2xl border-[15px] border-gray-900 bg-white p-4 text-black">
-          <div className="text-xl font-bold">{selectedExperience.title}</div>
-          <div className="text-sm font-semibold">{selectedExperience.job}</div>
-          <p className="mt-2 text-sm">{selectedExperience.information}</p>
+        <div className="flex h-[330px] w-[550px] items-center justify-center rounded-2xl bg-purple-300 shadow-[inset_0_2px_6px_rgba(0,0,0,0.2)]">
+          <div className="flex h-[300px] w-[500px] flex-col border-[8px] border-gray-900 bg-white p-4 text-black">
+            <div className="text-xl font-bold">{selectedExperience.title}</div>
+            <div className="text-sm font-semibold">
+              {selectedExperience.job}
+            </div>
+            <p className="mt-2 text-sm">{selectedExperience.information}</p>
+          </div>
         </div>
         <div className="flex h-full flex-col items-center justify-between py-20">
           <div className="grid h-16 w-16 rotate-45 grid-cols-2 gap-2">
@@ -41,7 +48,7 @@ const Experience = () => {
             ))}
           </div>
 
-          <div className="h-[70px] w-[70px] rounded-full border-8 border-white bg-gray-100"></div>
+          <div className="h-[70px] w-[70px] rounded-full border-8 border-white bg-gray-100 drop-shadow-[0_0_2px_black]"></div>
         </div>
       </div>
       <div>
