d65d828a9e59e4135c6d00dc46a8a41ac0384bfb
Simon <simoncun2586@gmail.com>
2025-06-27 10:37:17 -0700

    formatting

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -40,7 +40,7 @@ const About = () => {
   };
 
   return (
-    <div className="flex h-screen w-full items-center justify-center gap-8 border-b-2">
+    <div className="flex w-full items-center justify-center gap-8 border-b-2 py-4">
       <div className="h-[700px] w-[600px] rounded-4xl border-1 border-white/20 bg-blue-200/20 shadow-lg backdrop-blur-md backdrop-filter"></div>
       <div>
         <VerticalTitle title="ABOUT ME" />
