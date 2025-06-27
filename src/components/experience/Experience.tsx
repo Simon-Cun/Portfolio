a3aa863f745f309d93cd2b5afb27a71abc913eb5
Simon <simoncun2586@gmail.com>
2025-06-27 11:39:48 -0700

    forgot experinece linebreak

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -4,7 +4,7 @@ import { ExperienceData } from "@/data/experience";
 
 const Experience = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center py-20">
+    <div className="flex w-full flex-col items-center justify-center py-20 border-b-1">
       <HorizontalTitle title="Experience" />
       <div className="mt-20 w-[80%]">
         {ExperienceData.map((item, idx) => (
