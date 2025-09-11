d2391568e3aec1a8730ad7fc77b66354c6f71582
Simon <simoncun2586@gmail.com>
2025-09-10 22:26:57 -0700

    removed rounded

--- src/components/experience/ExperienceCard.tsx
+++ src/components/experience/ExperienceCard.tsx
@@ -19,23 +19,16 @@ const ExperienceCard = ({
   information,
 }: ExperienceCardProps) => {
   return (
-    <div className="w-full rounded-xl border-b border-white/20 bg-white/5 p-6 shadow-md backdrop-blur-md">
+    <div className="w-full border-b border-white/20 bg-white/5 p-6 shadow-md backdrop-blur-md">
       <div className="flex flex-col items-start gap-3 md:flex-row md:items-center md:justify-between">
         <div className="flex items-center gap-4">
-          <Image
-            src={logo}
-            alt={title}
-            width={100}
-            height={100}
-            className="rounded-md"
-          />
+          <Image src={logo} alt={title} width={100} height={100} />
           <div>
             <h3 className="text-lg font-semibold">{title}</h3>
             <p className="text-gray-400">{role}</p>
           </div>
         </div>
 
-        {/* Right: location above date */}
         <div className="mt-2 text-left text-gray-400 md:mt-0 md:text-right">
           <p className="">{location}</p>
           <p className="">{date}</p>
