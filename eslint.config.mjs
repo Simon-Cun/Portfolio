a60edff4345838b611e2387814c89b4bce82fcc3
Simon <simoncun2586@gmail.com>
2025-10-12 09:51:29 -0700

    modified eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-import globals from "globals";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import prettier from "eslint-plugin-prettier/recommended";
-import query from "@tanstack/eslint-plugin-query";
-
 export default [
   {
-    ignores: ["dist/*"],
+    ignores: [
+      "dist/*",
+      ".next/",
+      "out/",
+      "node_modules/",
+      "next-env.d.ts",
+    ],
   },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  {
-    ignores: [".next/", "out/", "node_modules"],
-  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: { ...globals.node } } },
   eslint.configs.recommended,
