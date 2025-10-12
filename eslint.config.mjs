3bc83f36b7a5a3096285fe9604466cdb4a778d25
Simon <simoncun2586@gmail.com>
2025-10-12 09:55:50 -0700

    ?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,22 +4,43 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import prettier from "eslint-plugin-prettier/recommended";
 import query from "@tanstack/eslint-plugin-query";
+
 export default [
-  { ignores: ["dist/*"] },
+  {
+    ignores: [
+      "dist/*",
+      ".next/",
+      "out/",
+      "node_modules/",
+      "next-env.d.ts",
+      "*.config.*js",
+    ],
+  },
+
+  // Apply to JavaScript/TypeScript source files
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { ignores: [".next/", "out/", "node_modules"] },
+
+  // CommonJS settings for plain .js files
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+
+  // Global variables (Node.js)
   { languageOptions: { globals: { ...globals.node } } },
+
+  // Recommended ESLint and TypeScript configs
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+
+  // Plugins
   {
     plugins: {
-      react: react,
-      prettier: prettier,
+      react,
+      prettier,
       "@typescript-eslint": tseslint.plugin,
       "@tanstack/query": query,
     },
   },
+
+  // Rules
   {
     rules: {
       "react/react-in-jsx-scope": "off",
@@ -29,5 +50,4 @@ export default [
       ],
     },
   },
-  { ignores: ["*.config.*js"] },
 ];
