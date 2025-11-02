75845893e1b4f9e82f344a84a29b4b9576a96058
Simon <simoncun2586@gmail.com>
2025-11-01 19:36:42 -0700

    removed about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Home from "@/components/home/Home";
-import About from "@/components/about/About";
 import Projects from "@/components/projects/Projects";
 import Experience from "@/components/experience/Experience";
 import Footer from "@/components/Footer";
@@ -8,7 +7,6 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center">
       <Home />
-      <About />
       <Projects />
       <Experience />
       <Footer />
